2a0d89cc8150e97ad0e99d0cf69bf10ea59ad4b7
Orlando Nunez <onunez@cam.com>
2019-06-27 15:15:52 -0400

    Proyect Created

---
+++ UniversalTranslator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversalTranslator_Onunez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

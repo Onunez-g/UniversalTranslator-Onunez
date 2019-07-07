1293a9eced46e6812ba6f994f865bcbe9bc7af71
ORLANDO DE JESUS NUNEZ GONZALEZ <nunez.orlando2@gmail.com>
2019-07-07 17:22:54 -0400

    UniversalTranslator version 1.0.0

--- UniversalTranslator/Program.cs
+++ UniversalTranslator/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using UniversalTranslator;
 
 namespace UniversalTranslator_Onunez
 {
     class Program
     {
+        static string path;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the path to the temperatures file:");
+            path = Console.ReadLine();
+            FileTemperature f = new FileTemperature();
+            var temps = f.ReadTemperatureFile(path);
+            f.ConvertValues(ref temps);
         }
     }
 }

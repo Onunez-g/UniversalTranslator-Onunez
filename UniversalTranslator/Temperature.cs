6e1a82b7b4e5cf7f6230e5de1fda6844e9f5c3cf
ORLANDO DE JESUS NUNEZ GONZALEZ <nunez.orlando2@gmail.com>
2019-07-07 13:43:34 -0400

    Creating Temperature Class

---
+++ UniversalTranslator/Temperature.cs
@@ -0,0 +1,10 @@
+namespace UniversalTranslator
+{
+    public class Temperature
+    {
+        public double value {get; set;}
+        public string actualUnit {get; set;}
+        public string resultUnit {get; set;}
+        public double? resultValue {get; set;}
+    }
+}
